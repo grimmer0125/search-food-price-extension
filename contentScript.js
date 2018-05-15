375d1b5de609e59a2717bf379d90ba83f893a956
Grimmer <grimmer0125@gmail.com>
2018-05-15 20:53:57 +0800

    Get product name and its price on sbd-ec.wellcome.com.tw without comparing with honestbee

--- contentScript.js
+++ contentScript.js
@@ -3,12 +3,14 @@ console.log("current page url:", window.location.href);
 
 const apiServer = "http://localhost:8080/rpc";
 const carrefourStoreURL = "online.carrefour.com.tw";
-
+const sbdStoreURL = "sbd-ec.wellcome.com.tw";
 const currentPage = window.location.href;
+
+let productName = null;
+let promotionPrice = null;
 
 if (currentPage.indexOf(carrefourStoreURL)>-1) {
 
-    let productName, promotionPrice;
     const productNameUI = document.querySelector(".pro-name");
     if (productNameUI) {
         productName = productNameUI.innerHTML;
@@ -22,52 +24,75 @@ if (currentPage.indexOf(carrefourStoreURL)>-1) {
         promotionPrice = promotionPrice.substring(1, promotionPrice.length);
     }
 
-    console.log("product name:", productName);
+} else if (currentPage.indexOf(sbdStoreURL)>-1) {
+    console.log("in sbdStoreURL site !!!!");
+    const productNameUI = document.querySelector(".product-name");
+    if (productNameUI) {
+        productName = productNameUI.innerHTML;
+    }
+    console.log("sbd product:", productName);
+
+    const priceUI = document.querySelector(".product-price-container");
+    if (priceUI) {
+        // alternative ways:
+        // https://stackoverflow.com/questions/16981134/how-to-get-text-of-element-but-excluding-a-child-of-that-element
+        // .nextSibling.nodeValue or .removeChild
+        promotionPrice = priceUI.querySelector(".item-price").textContent; //$49
+
+        promotionPrice = promotionPrice.substring(1, promotionPrice.length);
+    }
+
+    console.log("sbd price:", promotionPrice);
+
+    // TODO: since honestbee doest not have sbdStore products yet,
+    // so does not query it
+    promotionPrice = null;
+}
+
+
+if (productName && promotionPrice) {
+
+    console.log("query product name:", productName);
     console.log("product price:", promotionPrice);
 
-    if (productName && promotionPrice) {
-        // RPC like API
-        const body = {
-            method: "queryProduct",
-            params: {
-                store: "carrefour",
-                productName,
-                shownPrice: promotionPrice,
-                url: currentPage // notify internal staff when honestbee price is not cheaper
-            }
-        };
-        fetch(apiServer, {
-            method: 'POST',
-            body:JSON.stringify(body),
-        })
-        .then(res => {
-            return res.json();// text();
-        }).then(json => {
-            console.log("try to udpate UI:", json);
-            //TODO compare with promotionPrice
-            //if (json.Price && json.Price < promotionPrice) {
-            if (json.Price) {
-                // OK: update UI, e.g.
-                var a = document.createElement('a');
-                a.className = 'float Kachel';
+    // RPC like API
+    const body = {
+        method: "queryProduct",
+        params: {
+            store: "carrefour",
+            productName,
+            shownPrice: promotionPrice,
+            url: currentPage // notify internal staff when honestbee price is not cheaper
+        }
+    };
+    fetch(apiServer, {
+        method: 'POST',
+        body:JSON.stringify(body),
+    })
+    .then(res => {
+        return res.json();// text();
+    }).then(json => {
+        console.log("try to udpate UI:", json);
+        //TODO compare with promotionPrice
+        //if (json.Price && json.Price < promotionPrice) {
+        if (json.Price) {
+            // OK: update UI, e.g.
+            var a = document.createElement('a');
+            a.className = 'float Kachel';
 
-                //document.createElement('i');
-                var linkText = document.createTextNode("Bee:"+json.Price);
-                a.appendChild(linkText);
+            //document.createElement('i');
+            var linkText = document.createTextNode("Bee:"+json.Price);
+            a.appendChild(linkText);
 
-                // a.title = "my title text";
-                a.href = json.RemoteURL;
+            // a.title = "my title text";
+            a.href = json.RemoteURL;
 
-                document.body.appendChild(a);
-            } else {
-                //  case2 (not found), e.g.:
-                // {"PreviewImageUrl":"","Price":"","RemoteURL":
-                // "https://www.honestbee.tw/zh-TW/groceries/stores/carrefour/products/0",
-                // "Title":""}
-            }
-        });
-    }
-} else {
-
-
+            document.body.appendChild(a);
+        } else {
+            //  case2 (not found), e.g.:
+            // {"PreviewImageUrl":"","Price":"","RemoteURL":
+            // "https://www.honestbee.tw/zh-TW/groceries/stores/carrefour/products/0",
+            // "Title":""}
+        }
+    });
 }
